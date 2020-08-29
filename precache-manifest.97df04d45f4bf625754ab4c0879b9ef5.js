b84d7a6242db51181cec05e505eeee55ec1505ea
Elise Bal <m11elbal@gmail.com>
2020-08-29 14:18:53 -0500

    Updates

---
+++ precache-manifest.97df04d45f4bf625754ab4c0879b9ef5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fe704c81862367b61fd41676cd1d0f",
+    "url": "/Web-Trivia/index.html"
+  },
+  {
+    "revision": "b939e68fb462428b0b22",
+    "url": "/Web-Trivia/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "6ab5f394b91850cdd8d6",
+    "url": "/Web-Trivia/static/css/main.d541ee39.chunk.css"
+  },
+  {
+    "revision": "b939e68fb462428b0b22",
+    "url": "/Web-Trivia/static/js/2.8b8818e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Web-Trivia/static/js/2.8b8818e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab5f394b91850cdd8d6",
+    "url": "/Web-Trivia/static/js/main.a454c64a.chunk.js"
+  },
+  {
+    "revision": "056e49763ab57bb37b9b",
+    "url": "/Web-Trivia/static/js/runtime-main.c7c901e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Web-Trivia/static/media/logo.5d5d9eef.svg"
+  }
+]);
