14f06f8a3912aefafc21b990d88f41a154b0ce83
Elise Bal <m11elbal@gmail.com>
2020-08-29 14:08:50 -0500

    Updates

---
+++ precache-manifest.9e66028482147256b5450ddd866ca09e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089f3520a5eb73cf64de82e387962507",
+    "url": "/web-trivia/index.html"
+  },
+  {
+    "revision": "b939e68fb462428b0b22",
+    "url": "/web-trivia/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "6e7a392eaa6aa209c8d7",
+    "url": "/web-trivia/static/css/main.d541ee39.chunk.css"
+  },
+  {
+    "revision": "b939e68fb462428b0b22",
+    "url": "/web-trivia/static/js/2.8b8818e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/web-trivia/static/js/2.8b8818e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e7a392eaa6aa209c8d7",
+    "url": "/web-trivia/static/js/main.cb7e82f9.chunk.js"
+  },
+  {
+    "revision": "83242d57b0dbb161b8b9",
+    "url": "/web-trivia/static/js/runtime-main.b6e36269.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-trivia/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Web-Trivia/precache-manifest.97df04d45f4bf625754ab4c0879b9ef5.js"
+  "/web-trivia/precache-manifest.9e66028482147256b5450ddd866ca09e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Web-Trivia/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-trivia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
