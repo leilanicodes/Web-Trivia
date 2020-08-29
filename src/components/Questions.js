c022a0e3198f6a7c29d575edf559cb0761b8431f
Elise Bal <m11elbal@gmail.com>
2020-08-29 15:56:09 -0500

    gets category from route paramter

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { withRouter, NavLink } from 'react-router-dom';
+import { withRouter, NavLink, useParams } from 'react-router-dom';
+import { fetchQuestions } from '../redux/trivia';
 import { Spinner } from 'reactstrap';
 
 import ScoreModal from './Modal';
 
 export class Questions extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       numberCorrect: 0,
       loading: true,
     };
     this.handleChoice = this.handleChoice.bind(this);
   }
-  componentDidMount = () => {
-    setTimeout(() => {
-      this.setState({
-        loading: false,
-      });
-    }, 500);
+  componentDidMount = async () => {
+    const {
+      match: { params },
+    } = this.props;
+
+    await this.props.getQuestions(params.category);
+
+    this.setState({
+      loading: false,
+    });
   };
   handleChoice(choice, result, buttonId) {
     let button = document.getElementById(buttonId);
@@ -54,78 +59,76 @@ export class Questions extends React.Component {
     const results = this.props.results.results;
 
     return (
-      <div>
-        <div className="questions-wrapper trivia" key={results}>
-          <nav>
-            <NavLink className="nav-link" to="/">
-              X
-            </NavLink>
-          </nav>
-          {this.state.loading ? (
-            <Spinner
-              color="dark"
-              style={{
-                width: '4rem',
-                height: '4rem',
-              }}
-            />
-          ) : results && results.length ? (
-            results.map((result, questionIndex) => (
-              <div className="question" key={result.question}>
-                <h2>
-                  {questionIndex +
-                    1 +
-                    '. ' +
-                    result.question
-                      .replace(/(&quot\;)/g, '"')
-                      .replace(/&#039;/g, "'")
-                      .replace(/&lt;/g, '<')
-                      .replace(/&gt;/g, '>')}
-                </h2>
+      <div className="questions-wrapper trivia" key={results}>
+        <nav>
+          <NavLink className="nav-link" to="/">
+            X
+          </NavLink>
+        </nav>
+        {this.state.loading ? (
+          <Spinner
+            color="dark"
+            style={{
+              width: '4rem',
+              height: '4rem',
+            }}
+          />
+        ) : results && results.length ? (
+          results.map((result, questionIndex) => (
+            <div className="question" key={result.question}>
+              <h2>
+                {questionIndex +
+                  1 +
+                  '. ' +
+                  result.question
+                    .replace(/(&quot\;)/g, '"')
+                    .replace(/&#039;/g, "'")
+                    .replace(/&lt;/g, '<')
+                    .replace(/&gt;/g, '>')}
+              </h2>
 
-                <form id="choice-form">
-                  {result.shuffledAnswers.map((choice, buttonIndex) => (
-                    <div key={choice.incorrect_answers}>
-                      <button
-                        disabled={false}
-                        type="button"
-                        className="choice"
-                        choice={choice}
-                        dangerouslySetInnerHTML={{ __html: choice }}
-                        id={questionIndex + '-' + buttonIndex}
-                        onClick={() => {
-                          this.handleChoice(
-                            choice,
-                            result,
-                            questionIndex + '-' + buttonIndex
-                          );
-                        }}
-                      >
-                        {/* {choice
+              <form id="choice-form">
+                {result.shuffledAnswers.map((choice, buttonIndex) => (
+                  <div key={choice.incorrect_answers}>
+                    <button
+                      disabled={false}
+                      type="button"
+                      className="choice"
+                      choice={choice}
+                      dangerouslySetInnerHTML={{ __html: choice }}
+                      id={questionIndex + '-' + buttonIndex}
+                      onClick={() => {
+                        this.handleChoice(
+                          choice,
+                          result,
+                          questionIndex + '-' + buttonIndex
+                        );
+                      }}
+                    >
+                      {/* {choice
                             .replace(/&quot;/g, '"')
                             .replace(/&#039;/g, "'")
                             .replace(/&lt;/g, '<')
                             .replace(/&gt;/g, '>')
                             .replace(/&lrm;/g, '')
                             .replace(/&oacute;/g, 'ó')} */}
-                      </button>
-                    </div>
-                  ))}
-                </form>
-              </div>
-            ))
-          ) : (
-            <div>
-              <h2>A category has not been selected yet.</h2>
+                    </button>
+                  </div>
+                ))}
+              </form>
             </div>
-          )}
-          {!this.state.loading && results && results.length && (
-            <ScoreModal
-              buttonLabel="Check Your Score"
-              score={this.state.numberCorrect * 10}
-            />
-          )}
-        </div>
+          ))
+        ) : (
+          <div>
+            <h2>A category has not been selected yet.</h2>
+          </div>
+        )}
+        {!this.state.loading && results && results.length && (
+          <ScoreModal
+            buttonLabel="Check Your Score"
+            score={this.state.numberCorrect * 10}
+          />
+        )}
       </div>
     );
   }
@@ -137,4 +140,8 @@ const mapState = (reduxState) => {
   };
 };
 
-export default withRouter(connect(mapState)(Questions));
+const mapDispatch = (dispatch) => ({
+  getQuestions: (category) => dispatch(fetchQuestions(category)),
+});
+
+export default withRouter(connect(mapState, mapDispatch)(Questions));
